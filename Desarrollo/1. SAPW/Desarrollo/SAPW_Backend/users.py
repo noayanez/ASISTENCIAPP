e1b75ee7ea77eaf10d4b72325892c0c5f8eb0309
gonzalotoledo <gonzalotolaran@gmail.com>
2018-11-18 23:38:00 -0500

    anadiendo lista de usuarios

--- Desarrollo/1. SAPW/Desarrollo/SAPW_Backend/users.py
+++ Desarrollo/1. SAPW/Desarrollo/SAPW_Backend/users.py
@@ -26,28 +26,53 @@ parser.add_argument('password')
 
 
 user_fields = {
+	'id' : fields.Integer,
 	'username' : fields.String,
 	'password' : fields.String,
 }
 
+
+class User(Resource):
+	@marshal_with(user_fields, envelope='user')
+	def get(self, idUser):
+		user = Users.query.filter_by(id=idUser).first()
+		return user
+
+	def delete(self, idUser):
+		user = Users.query.filter_by(id=idUser).first()
+		db.session.delete(user)
+		db.session.commit()
+		return '', 204
+
+	@marshal_with(user_fields, envelope='user')
+	def put(self, idUser):
+		args = parser.parse_args()
+		user = Users.query.filter_by(id=idUser).first()
+		if args['username'] != None:
+			user.username = args['username']
+		
+		if args['password'] != None:
+			user.password = args['password']
+		
+		db.session.add(user)
+		db.session.commit()
+		return user, 201
+
 class UsersList(Resource):
 	@marshal_with(user_fields, envelope='users')
 	def get(self, **kwargs):
-		#return Users.query.filter_by(id=1).first()
-		print(Users.query.all())
 		return Users.query.all()
 
 	@marshal_with(user_fields, envelope='user')
 	def post(self):
 		args = parser.parse_args()
 		new_user = Users(username=args['username'], password=args['password'])
-		print(new_user)
 		db.session.add(new_user)
 		db.session.commit()
 		return new_user, 201
 
 api.add_resource(UsersList, '/users')
-
+api.add_resource(User, '/users/<idUser>')
 
 if __name__ == '__main__':
     db.create_all()
